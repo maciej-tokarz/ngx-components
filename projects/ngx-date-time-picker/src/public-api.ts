1d56404db813522c8b0ae55a2bd9f6aec74d960e
maciej-tokarz <maciej.tokarz@my-poi.pl>
2022-04-10 18:11:27 +0200

    remove unused service

--- projects/ngx-date-time-picker/src/public-api.ts
+++ projects/ngx-date-time-picker/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-date-time-picker
  */
 
-// export * from './lib/ngx-date-time-picker.service';
 export * from './lib/ngx-date-time-picker.component';
 export * from './lib/ngx-date-time-picker.module';
